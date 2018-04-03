b32988cb2d05c9a286b5c1eef8093a697133f16d
aitorg1998 <aitorgarber@gmail.com>
2018-04-03 17:56:58 +0200

    Creada rama master

---
+++ src/vehiculos/Vehicle.java
@@ -0,0 +1,9 @@
+package vehiculos;
+
+public class Vehicle {
+	private static int VELOCIDAD_MAXIMA = 120;
+	public void Accelerar(){
+		int cilindrada, puertas, velocidad_actual=0;
+		velocidad_actual=velocidad_actual+10;
+	}
+}
