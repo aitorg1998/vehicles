c416458eafd847ee873688674b86d868e6cc3540
aitorg1998 <aitorgarber@gmail.com>
2018-04-09 16:07:24 +0200

    Creado constructor clase

--- src/vehiculos/Vehicle.java
+++ src/vehiculos/Vehicle.java
@@ -3,6 +3,12 @@ package vehiculos;
 public class Vehicle {
 	private static int VELOCIDAD_MAXIMA = 120;
 	public int cilindrada, puertas, velocidad_actual;
+	
+	public Vehicle() {
+
+		this.velocidad_actual = 0;
+	}
+
 	public void Accelerar(){
 		
 		velocidad_actual=velocidad_actual+10;
