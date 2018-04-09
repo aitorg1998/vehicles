e3184de3d44eef4da92fe477ddc807d5ea269884
aitorg1998 <aitorgarber@gmail.com>
2018-04-09 16:02:28 +0200

    Cambio variables de lugar

--- src/vehiculos/Vehicle.java
+++ src/vehiculos/Vehicle.java
@@ -2,8 +2,9 @@ package vehiculos;
 
 public class Vehicle {
 	private static int VELOCIDAD_MAXIMA = 120;
+	public int cilindrada, puertas, velocidad_actual;
 	public void Accelerar(){
-		int cilindrada, puertas, velocidad_actual;
+		
 		velocidad_actual=velocidad_actual+10;
 	}
 }
