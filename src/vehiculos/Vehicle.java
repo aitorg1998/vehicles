7289177408a309e1b2caa4210c8ef4059e3ffbb1
aitorg1998 <aitorgarber@gmail.com>
2018-04-09 15:44:43 +0200

    velocidad actual (quitada inicializacion a 0)

--- src/vehiculos/Vehicle.java
+++ src/vehiculos/Vehicle.java
@@ -3,7 +3,7 @@ package vehiculos;
 public class Vehicle {
 	private static int VELOCIDAD_MAXIMA = 120;
 	public void Accelerar(){
-		int cilindrada, puertas, velocidad_actual=0;
+		int cilindrada, puertas, velocidad_actual;
 		velocidad_actual=velocidad_actual+10;
 	}
 }
